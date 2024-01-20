2daa6745ee1457014837f612ac0e192a029df4f1
Borovail <skorikdaniil17@gmail.com>
2024-01-20 17:20:22 +0200

    Basic monster intelligence
    may not be the best and most efficient solution, but it works.

--- Assets/Scripts/Monster.cs
+++ Assets/Scripts/Monster.cs
@@ -1,8 +1,82 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Monster : MonoBehaviour
 {
-   
+    public float triggerRange;
+    public float chaseRange;
+    public float chaseSpeed;
+    public float returningSpeed;
+
+    public LayerMask playerLayer;
+
+    private Vector3 triggerPoint;
+    private Vector3 chaseDirection;
+    private Vector3 playerPosition;
+
+
+    private bool isTriggered = false;
+
+    private Collider2D monsterCollider;
+    private ContactFilter2D contactFilter;
+    private Collider2D[] collidersResult;
+    private void Awake()
+    {
+        triggerPoint = transform.position;
+
+        monsterCollider = GetComponent<Collider2D>();
+        contactFilter.SetLayerMask(playerLayer);
+        contactFilter.useLayerMask = true;
+        collidersResult = new Collider2D[1];
+    }
+
+    private void Start()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        playerPosition = GameManager.instance.player.transform.position;
+        float sqrDistanceToPlayer = (triggerPoint - playerPosition).sqrMagnitude;
+
+        if (sqrDistanceToPlayer < triggerRange * triggerRange)
+        {
+            isTriggered = true;
+        }
+
+        if (isTriggered)
+        {
+            if (sqrDistanceToPlayer < chaseRange * chaseRange)
+                ChasePlayer();
+            else
+                ReturnToTriggerPoint();
+        }
+    }
+
+    private void ChasePlayer()
+    {
+       int  collidersCount= Physics2D.OverlapCollider(monsterCollider, contactFilter, collidersResult);
+        if (collidersCount != 0) return;
+
+           chaseDirection = (playerPosition - transform.position).normalized;
+            transform.position += chaseDirection * chaseSpeed * Time.deltaTime;
+
+    }
+
+    private void ReturnToTriggerPoint()
+    {
+        chaseDirection = (triggerPoint - transform.position).normalized;
+        transform.position += chaseDirection * returningSpeed * Time.deltaTime;
+
+        if (Vector3.Distance(transform.position, triggerPoint) < 0.1f)
+            isTriggered = false;
+
+    }
+
+
+
+
 }
--- Assets/Scripts/Sword.cs
+++ Assets/Scripts/Sword.cs
@@ -6,7 +6,7 @@ public class Sword : MonoBehaviour
 {
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        collision.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector3(-1,0,0) * 0.4f, ForceMode2D.Impulse);
+        //collision.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector3(-1,0,0) * 0.4f, ForceMode2D.Impulse);
     }
 
  
