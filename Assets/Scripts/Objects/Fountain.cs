5b7541ab8c64a4fd8667b01f14a6c346a4ad16af
Borovail <skorikdaniil17@gmail.com>
2024-01-27 14:52:28 +0200

    Fountain2.0

---
+++ Assets/Scripts/Objects/Fountain.cs
@@ -0,0 +1,77 @@
+﻿using Assets.Scripts.Managers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class Fountain : MonoBehaviour
+{
+    public float effectCooldown = 5f;
+    public FountainType fountainType;
+    public Animator[] torchAnimators;
+
+    public AnimationClip[] torchAnimationClips;
+
+    private float startTime = 0f;
+    private bool isFountainTriggered = false;
+
+    private void Start()
+    {
+       TurnOffTorches();
+    }
+
+
+    private void Update()
+    {
+        if (isFountainTriggered)
+        {
+            startTime -= Time.deltaTime;
+
+            if (startTime <= 0)
+            {
+                startTime = 0;
+                isFountainTriggered = false;
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        EffectManager.Instance.ApplyFountainEffect(fountainType,startTime,effectCooldown,transform.position  + new Vector3( 0.25f,-0.25f,0f));
+        TurnOnTorches();
+        startTime = effectCooldown;
+        isFountainTriggered = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        EffectManager.Instance.CancelFountainEffect(fountainType);
+        TurnOffTorches();
+
+    }
+
+
+    private void TurnOffTorches()
+    {
+        foreach (var animator in torchAnimators)
+        {
+            animator.Play(torchAnimationClips[0].name);
+        }
+    }
+
+    private void TurnOnTorches()
+    {
+        foreach (var animator in torchAnimators)
+        {
+            animator.Play(torchAnimationClips[1].name);
+        }
+    }
+}
+
+public enum FountainType
+{
+    HealthRegeneration,
+    ArmorRegeneration
+}
